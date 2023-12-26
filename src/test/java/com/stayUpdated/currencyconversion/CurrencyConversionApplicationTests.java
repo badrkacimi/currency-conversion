0868576188a24b56c12ce160663858629fe6ace6
badrkacimi <badrvkacimi@gmail.com>
2023-12-26 01:35:08 +0100

    Initial commit add controller and restTemplate add bean and properties

---
+++ src/test/java/com/stayUpdated/currencyconversion/CurrencyConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayUpdated.currencyconversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
